21eb57d76107845ed749a334510e1cc4c304afe0
Royber <royber2020@gmail.com>
2022-07-22 15:14:46 -0300

    Add project files.

---
+++ Goldlizze/Goldlizze/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Goldlizze.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Goldlizze.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Goldlizze/Goldlizze/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Goldlizze.Models;
+using Goldlizze.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Goldlizze.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
